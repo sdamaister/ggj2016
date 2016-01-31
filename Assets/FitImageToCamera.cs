a7f51e738dd2993a168758deccde022c048fa15a
Sergio Basurco <sergio.basurco@gmail.com>
2016-01-31 15:12:32 +0100

    Scenes flow and times set.

---
+++ Assets/FitImageToCamera.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FitImageToCamera : MonoBehaviour {
+
+	private SpriteRenderer sprite;
+
+	private float width;
+	private float height;
+	private float aspect;
+
+	private float screenWidthWorld;
+	private float screenHeightWorld;
+
+
+	float worldScreenHeight;
+	float worldScreenWidth;
+	float spriteScaleWidth; 
+
+	public float totalOffset;
+
+
+	// Use this for initialization
+	void Start () {
+		sprite = GetComponent<SpriteRenderer>();
+		sprite.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, sprite.transform.position.z);
+		width = sprite.sprite.bounds.size.x;
+		height = sprite.sprite.bounds.size.y;
+		aspect = width/height;
+
+		worldScreenHeight = Camera.main.orthographicSize * 2.0f;
+
+		float screenAspect = (float)Screen.width / (float) Screen.height;
+
+
+		if(screenAspect > aspect)
+		{
+			worldScreenWidth = worldScreenHeight * Screen.width/Screen.height;
+			spriteScaleWidth = worldScreenHeight * aspect;
+			ResizeSpriteToScreen(spriteScaleWidth / width, worldScreenHeight / height);
+		}else
+		{
+			float w = Camera.main.orthographicSize * 2.0f;
+			float h = w / aspect;
+			ResizeSpriteToScreen(w / width, h / height);
+		}
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+	void ResizeSpriteToScreen(float width, float height) {
+
+		Vector3 newLocalScale = new Vector3(width, height);
+		transform.localScale = newLocalScale;
+
+	}
+
+
+}
---
+++ Assets/Scripts/GameFlow/SplashAndLoad.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SplashAndLoad : MonoBehaviour {
+	public string sceneToLoad;
+    public float TimeToMakeTransition = 3;
+
+    private float elapsedTime;
+
+	// Use this for initialization
+	void Start () {
+        elapsedTime = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime >= TimeToMakeTransition)
+        {
+			SceneManager.LoadScene(sceneToLoad);
+        }
+	}
+}
